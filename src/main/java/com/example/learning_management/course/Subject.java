ad767a7db3b0cdddeebdfaec95c95c324e3c23c4
PhamPhien-gif <toilaphien2302@gmail.com>
2026-02-17 23:59:43 +0700

    nothing

--- src/main/java/com/example/learning_management/course/Subject.java
+++ src/main/java/com/example/learning_management/course/Subject.java
@@ -1,20 +1,13 @@
 package com.example.learning_management.course;
 
 import java.util.List;
-import java.util.UUID;
-
 import com.example.learning_management.shared.BaseEntity;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
